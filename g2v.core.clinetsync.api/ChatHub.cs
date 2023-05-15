c9058f116e1586e41be596029d9f4f5153082bdc
kamran bashir <kamran_111@yahoo.co.uk>
2023-05-15 17:53:23 +0500

    using hang fire

--- g2v.core.clinetsync.api/ChatHub.cs
+++ g2v.core.clinetsync.api/ChatHub.cs
@@ -12,45 +12,31 @@ namespace g2v.core.clinetsync.api
 {
     public class ChatHub : Hub
     {
-        private System.Threading.Timer? timer;
+
         private readonly IHubContext<ChatHub> _context;
-        private readonly IBackgroundJobClient _jobClient;
-        public ChatHub(IHubContext<ChatHub> hubContext, IBackgroundJobClient jobClient)
+
+        public ChatHub(IHubContext<ChatHub> hubContext)
         {
             _context = hubContext;
-            _jobClient = jobClient;
+            
         }
         public async Task SendMessage(string user, string message)
         {
+            RecurringJob.AddOrUpdate("chartsjob", ()=> GetCurrentUserNotifications(),Cron.Minutely);
 
-            timer = new System.Threading.Timer(DoWork, null, TimeSpan.FromSeconds(3),
-                 TimeSpan.FromSeconds(1));
-
-            _jobClient.Schedule(() =>  GetCurrentUserNotifications(),   TimeSpan.FromSeconds(2));
-
-            RecurringJob.AddOrUpdate("chartsjob",     () => GetCurrentUserNotifications(),     Cron.Minutely);
-
-            await Clients.All.SendAsync("ReceiveMessage", user,timer.GetType().Name);
+            await Clients.All.SendAsync("ReceiveMessage2", user,message);
         }
-
-        private void DoWork(object? state)
-        {
-            Console.WriteLine("aaa");
-            GetCurrentUserNotifications();
-            //_context.Clients.All.SendAsync("ReceiveMessage", DateTime.Now.Ticks, DateTime.Now.Ticks);
-        }
-
         public void GetCurrentUserNotifications()
         {
-            int[] arr = getArray();
+            int[] arr = GetArray();
             _context.Clients.All.SendAsync("ReceiveMessage", arr);
         }
 
-        private static int[] getArray()
+        private static int[] GetArray()
         {
             int Min = 0;
             int Max = 20;
-            Random randNum = new Random();
+            var randNum = new Random();
             int[] arr = Enumerable
                 .Repeat(0, 100)
                 .Select(i => randNum.Next(Min, Max))
